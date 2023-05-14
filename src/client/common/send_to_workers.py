9e8a4d50bf93e70e1ba78cabd13aac713ef62509
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-05-14 15:52:05 -0300

    middleware in client

--- src/client/common/send_to_workers.py
+++ src/client/common/send_to_workers.py
@@ -1,10 +1,7 @@
 from .config import config
-
-
-def publish_to_workers(data):
-  config.pub_socket.send_string(data)
+import middleware
 
 
 def send_to_all_workers(data):
   for i in range(config.workers_amount):
-    config.push_socket.send_string(data)
+    middleware.push(data)
